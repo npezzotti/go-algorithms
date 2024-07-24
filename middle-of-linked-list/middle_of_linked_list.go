5624ab5973e400268336226f4e2cc49ca2eabfff
npezzotti <nathan.pezzotti@hashicorp.com>
2024-07-23 20:45:07 -0400

    adds middle of linked list

---
+++ middle-of-linked-list/middle_of_linked_list.go
@@ -0,0 +1,17 @@
+package middle_of_linked_list
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
---
+++ middle-of-linked-list/middle_of_linked_list_test.go
@@ -0,0 +1,59 @@
+package middle_of_linked_list
+
+import "testing"
+
+func TestMiddleNode(t *testing.T) {
+	tcases := []struct {
+		name string
+		head *ListNode
+		want int
+	}{
+		{
+			name: "odd list",
+			head: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val: 4,
+							Next: &ListNode{
+								Val:  5,
+								Next: nil,
+							},
+						},
+					},
+				},
+			},
+			want: 3,
+		},
+		{
+			name: "even list",
+			head: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val:  4,
+							Next: nil,
+						},
+					},
+				},
+			},
+			want: 3,
+		},
+	}
+
+	for _, tc := range tcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := middleNode(tc.head)
+
+			if got.Val != tc.want {
+				t.Fatalf("expected 3, got %d\n", got.Val)
+			}
+		})
+	}
+}
