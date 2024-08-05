c35e46c0fa0e7e986f1d68ca2abd28841a4cd20b
npezzotti <nathan.pezzotti@hashicorp.com>
2024-08-04 22:27:31 -0400

    adds more tests

--- zero_one_matrix/zero_one_matrix_test.go
+++ zero_one_matrix/zero_one_matrix_test.go
@@ -6,10 +6,26 @@ import (
 )
 
 func TestUpdateMatrix(t *testing.T) {
-	got := updateMatrix([][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}})
-	want := [][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
+	testCases := map[string]struct {
+		matrix [][]int
+		want   [][]int
+	}{
+		"one": {
+			matrix: [][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}},
+			want:   [][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}},
+		},
+		"two": {
+			matrix: [][]int{{0, 0, 0}, {0, 1, 0}, {1, 1, 1}},
+			want:   [][]int{{0, 0, 0}, {0, 1, 0}, {1, 2, 1}},
+		},
+	}
+	for tn, tc := range testCases {
+		t.Run(tn, func(t *testing.T) {
+			got := updateMatrix(tc.matrix)
 
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("got %v, want %v\n", got, want)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("got %v, want %v\n", got, tc.want)
+			}
+		})
 	}
 }
