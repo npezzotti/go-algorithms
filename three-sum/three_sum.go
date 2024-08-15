02108a0c86e3978367837aa6ca6dec1ee4e56cf4
npezzotti <nathan.pezzotti@hashicorp.com>
2024-08-15 14:48:42 -0400

    adds three sum

---
+++ three-sum/three_sum.go
@@ -0,0 +1,35 @@
+package three_sum
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+
+	var res = make([][]int, 0)
+	for idx1 := 0; idx1 < len(nums)-2; idx1++ {
+		if idx1 > 0 && nums[idx1] == nums[idx1-1] {
+			continue
+		}
+
+		idx2 := idx1 + 1
+		idx3 := n - 1
+		for idx2 < idx3 {
+			sum := nums[idx1] + nums[idx2] + nums[idx3]
+			if sum == 0 {
+				res = append(res, []int{nums[idx1], nums[idx2], nums[idx3]})
+				idx3--
+
+				for idx2 < idx3 && nums[idx2] == nums[idx3]+1 {
+					idx3--
+				}
+			} else if sum > 0 {
+				idx3--
+			} else {
+				idx2++
+			}
+		}
+	}
+
+	return res
+}
---
+++ three-sum/three_sum_test.go
@@ -0,0 +1,36 @@
+package three_sum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestThreeSum(t *testing.T) {
+	testCases := map[string]struct {
+		nums []int
+		want [][]int
+	}{
+		"two triplets sum to zero": {
+			nums: []int{-1, 0, 1, 2, -1, -4},
+			want: [][]int{{-1, -1, 2}, {-1, 0, 1}},
+		},
+		"one triplet does not sum to 0": {
+			nums: []int{0, 1, 1},
+			want: [][]int{},
+		},
+		"one triplet sums to 0": {
+			nums: []int{0, 0, 0},
+			want: [][]int{{0, 0, 0}},
+		},
+	}
+
+	for tn, tc := range testCases {
+		t.Run(tn, func(t *testing.T) {
+			got := threeSum(tc.nums)
+
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("got %+v, want %+v", got, tc.want)
+			}
+		})
+	}
+}
