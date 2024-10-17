c0a50c63c71805b202edd61c4fb0ea67c8cb41d2
npezzotti <nathan.pezzotti@hashicorp.com>
2024-10-17 08:26:48 -0400

    uncomments other test cases

--- atoi/atoi_test.go
+++ atoi/atoi_test.go
@@ -7,42 +7,42 @@ func TestMyAtoi(t *testing.T) {
 		input  string
 		output int
 	}{
-		// {
-		// 	input:  " ",
-		// 	output: 0,
-		// },
-		// {
-		// 	input:  "42",
-		// 	output: 42,
-		// },
-		// {
-		// 	input:  "   -042",
-		// 	output: -42,
-		// },
-		// {
-		// 	input:  "1337c0d3",
-		// 	output: 1337,
-		// },
-		// {
-		// 	input:  "0-1",
-		// 	output: 0,
-		// },
-		// {
-		// 	input:  "words and 987",
-		// 	output: 0,
-		// },
-		// {
-		// 	input:  "-91283472332",
-		// 	output: -2147483648,
-		// },
+		{
+			input:  " ",
+			output: 0,
+		},
+		{
+			input:  "42",
+			output: 42,
+		},
+		{
+			input:  "   -042",
+			output: -42,
+		},
+		{
+			input:  "1337c0d3",
+			output: 1337,
+		},
+		{
+			input:  "0-1",
+			output: 0,
+		},
+		{
+			input:  "words and 987",
+			output: 0,
+		},
+		{
+			input:  "-91283472332",
+			output: -2147483648,
+		},
 		{
 			input:  "9223372036854775808",
 			output: 2147483647,
 		},
-		// {
-		// 	input:  "+-12",
-		// 	output: 0,
-		// },
+		{
+			input:  "+-12",
+			output: 0,
+		},
 	}
 
 	for _, tc := range tcases {
