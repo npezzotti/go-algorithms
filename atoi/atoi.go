40dfc6d59550fc524f93d8475ada0c1c97c28ff7
npezzotti <nathan.pezzotti@hashicorp.com>
2024-10-16 22:58:23 -0400

    adds atoi

---
+++ atoi/atoi.go
@@ -0,0 +1,46 @@
+package atoi
+
+import (
+	"math"
+)
+
+func myAtoi(s string) int {
+	var res, idx int
+	var negative bool
+	n := len(s)
+
+	for idx < n && s[idx] == ' ' {
+		idx++
+	}
+
+	if idx < n && (s[idx] == '+' || s[idx] == '-') {
+		if s[idx] == '-' {
+			negative = true
+		}
+		idx++
+	}
+
+	for ; idx < n; idx++ {
+		digit := int(s[idx] - '0')
+
+		if digit < 0 || digit > 9 {
+			break
+		}
+
+		res = res*10 + digit
+	}
+
+	if res > math.MaxInt32 && !negative {
+		return math.MaxInt32
+	}
+
+	if -res < math.MinInt32 && negative {
+		return math.MinInt32
+	}
+
+	if negative {
+		res = -res
+	}
+
+	return res
+}
