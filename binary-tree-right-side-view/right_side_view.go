0eafa4a9e16d8b88655fbe53a61463713062b86f
npezzotti <nathan.pezzotti@hashicorp.com>
2024-10-28 09:15:38 -0400

    adds right side view

---
+++ binary-tree-right-side-view/right_side_view.go
@@ -0,0 +1,55 @@
+package right_side_view
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rightSideViewDFS(root *TreeNode) []int {
+	res := make([]int, 0)
+
+	var dfs func(node *TreeNode, level int)
+	dfs = func(node *TreeNode, level int) {
+		if node == nil {
+			return
+		}
+
+		if level == len(res) {
+			res = append(res, node.Val)
+		}
+
+		dfs(node.Right, level+1)
+		dfs(node.Left, level+1)
+	}
+
+	dfs(root, 0)
+
+	return res
+}
+
+func rightSideViewBFS(root *TreeNode) []int {
+	res := make([]int, 0)
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		queueLen := len(queue)
+		res = append(res, queue[queueLen-1].Val)
+
+		for i := 0; i < queueLen; i++ {
+			cur := queue[0]
+			queue = queue[1:]
+
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+			}
+
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+			}
+		}
+	}
+	
+	return res
+}
---
+++ binary-tree-right-side-view/right_side_view_test.go
@@ -0,0 +1,71 @@
+package right_side_view
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestRightSideView(t *testing.T) {
+	tcases := []struct {
+		tree   *TreeNode
+		output []int
+	}{
+		{
+			tree: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val:   2,
+					Right: &TreeNode{Val: 5},
+				},
+				Right: &TreeNode{
+					Val:   3,
+					Right: &TreeNode{Val: 4},
+				},
+			},
+			output: []int{1, 3, 4},
+		},
+		{
+			tree: &TreeNode{
+				Val:   1,
+				Right: &TreeNode{Val: 3},
+			},
+			output: []int{1, 3},
+		},
+		{
+			tree: &TreeNode{
+				Val:  1,
+				Left: &TreeNode{Val: 2},
+			},
+			output: []int{1, 2},
+		},
+		{
+			tree: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val:  2,
+					Left: &TreeNode{Val: 4},
+				},
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},
+			output: []int{1, 3, 4},
+		},
+	}
+
+	t.Run("rightSideViewDFS", func(t *testing.T) {
+		for _, tc := range tcases {
+			if output := rightSideViewDFS(tc.tree); !slices.Equal(tc.output, output) {
+				t.Fatalf("rightSideViewDFS: expected %+v, got %+v", tc.output, output)
+			}
+		}
+	})
+
+	t.Run("rightSideViewBFS", func(t *testing.T) {
+		for _, tc := range tcases {
+			if output := rightSideViewBFS(tc.tree); !slices.Equal(tc.output, output) {
+				t.Fatalf("rightSideViewBFS: expected %+v, got %+v", tc.output, output)
+			}
+		}
+	})
+}
