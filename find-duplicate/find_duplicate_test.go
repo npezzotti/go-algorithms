f220f395c3bb3d58cb72089dee923658d70d79a7
npezzotti <nathan.pezzotti@hashicorp.com>
2024-12-15 13:49:21 -0500

    adds find duplicate

---
+++ find-duplicate/find_duplicate_test.go
@@ -0,0 +1,29 @@
+package find_duplicate
+
+import "testing"
+
+func Test_funcDuplicate(t *testing.T) {
+	tcases := []struct {
+		nums   []int
+		output int
+	}{
+		{
+			nums:   []int{1, 3, 4, 2, 2},
+			output: 2,
+		},
+		{
+			nums:   []int{3, 1, 3, 4, 2},
+			output: 3,
+		},
+		{
+			nums:   []int{3, 3, 3, 3, 3},
+			output: 3,
+		},
+	}
+
+	for _, tc := range tcases {
+		if output := findDuplicate(tc.nums); output != tc.output {
+			t.Errorf("got %d, expected %d", output, tc.output)
+		}
+	}
+}
