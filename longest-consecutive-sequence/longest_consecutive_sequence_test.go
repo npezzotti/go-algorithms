1c17ffffd216aa7adaae15e87ae84eafc12cc556
npezzotti <nathan.pezzotti@hashicorp.com>
2024-12-28 20:18:29 -0500

    uncomments test case

--- longest-consecutive-sequence/longest_consecutive_sequence_test.go
+++ longest-consecutive-sequence/longest_consecutive_sequence_test.go
@@ -7,10 +7,10 @@ func Test_longestConsecutive(t *testing.T) {
 		nums   []int
 		output int
 	}{
-		// {
-		// 	nums:   []int{100, 4, 200, 1, 3, 2},
-		// 	output: 4,
-		// },
+		{
+			nums:   []int{100, 4, 200, 1, 3, 2},
+			output: 4,
+		},
 		{
 			nums:   []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1},
 			output: 9,
