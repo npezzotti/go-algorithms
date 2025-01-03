6dd802c018c7fe059f3c33b6c3833e36949e0dfb
npezzotti <nathan.pezzotti@hashicorp.com>
2025-01-03 13:19:59 -0500

    uses formula in question description

--- find-k-closest-elements/find_k_closest_elements.go
+++ find-k-closest-elements/find_k_closest_elements.go
@@ -1,12 +1,14 @@
 package find_k_closest_elements
 
+import "math"
+
 func findClosestElements(arr []int, k int, x int) []int {
 	l, r := 0, len(arr)-k
 
 	for l < r {
 		mid := (l + r) >> 1
 
-		if x-arr[mid] <= arr[mid+k]-x {
+		if math.Abs(float64(x-arr[mid])) <= math.Abs(float64(x-arr[mid+k])) {
 			r = mid
 		} else {
 			l = mid + 1
