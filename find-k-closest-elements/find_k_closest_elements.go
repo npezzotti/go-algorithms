02b67681c29da72a7a4db17e49c2257abec3204c
npezzotti <nathan.pezzotti@hashicorp.com>
2025-01-03 12:55:50 -0500

    removes log line

--- find-k-closest-elements/find_k_closest_elements.go
+++ find-k-closest-elements/find_k_closest_elements.go
@@ -1,12 +1,9 @@
 package find_k_closest_elements
 
-import "fmt"
-
 func findClosestElements(arr []int, k int, x int) []int {
 	l, r := 0, len(arr)-k
 
 	for l < r {
-		fmt.Println(l, r)
 		mid := (l + r) >> 1
 
 		if x-arr[mid] <= arr[mid+k]-x {
