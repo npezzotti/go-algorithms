9f5890ec7958f33c9b87e05fb88d20157290a1b8
npezzotti <nathan.pezzotti@hashicorp.com>
2025-01-06 17:09:52 -0500

    calculates carry

--- add-two-numbers/add_two_numbers.go
+++ add-two-numbers/add_two_numbers.go
@@ -8,42 +8,25 @@ func addTwoNumbers(l1 *utils.ListNode, l2 *utils.ListNode) *utils.ListNode {
 	res := &utils.ListNode{}
 	curNode := res
 
-	var carry bool
-	for l1 != nil || l2 != nil || carry {
-		val1, val2 := 0, 0
+	var carry int
+	for l1 != nil || l2 != nil || carry > 0 {
+		sum := 0
 		if l1 != nil {
-			val1 = l1.Val
-		}
-
-		if l2 != nil {
-			val2 = l2.Val
-		}
-
-		sum := val1 + val2
-
-		if carry {
-			sum += 1
-			carry = false
-		}
-
-		if sum > 9 {
-			carry = true
-			sum = sum - 10
-		}
-
-		curNode.Next = &utils.ListNode{
-			Val: sum,
-		}
-
-		curNode = curNode.Next
-
-		if l1 != nil {
+			sum += l1.Val
 			l1 = l1.Next
 		}
 
 		if l2 != nil {
+			sum += l2.Val
 			l2 = l2.Next
 		}
+
+		sum += carry
+		carry = sum / 10
+		sum %= 10
+
+		curNode.Next = &utils.ListNode{Val: sum,}
+		curNode = curNode.Next
 	}
 
 	return res.Next
