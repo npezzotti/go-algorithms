f5d719bcc1de3260e10600bad9de98208278f1b7
npezzotti <nathan.pezzotti@hashicorp.com>
2025-01-07 12:11:52 -0500

    adds generate parenthesis

--- generate-parenthesis/generate_parenthesis.go
+++ generate-parenthesis/generate_parenthesis.go
@@ -1,5 +1,25 @@
 package generate_parenthesis
 
 func generateParenthesis(n int) []string {
-	
+	var res []string
+
+	var dfs func(l, r int, curRes string)
+	dfs = func(l, r int, curRes string) {
+		if l >= n && r >= n {
+			res = append(res, curRes)
+			return
+		}
+
+		if l < n {
+			dfs(l+1, r, curRes+"(")
+		}
+
+		if r < l {
+			dfs(l, r+1, curRes+")")
+		}
+	}
+
+	dfs(0, 0, "")
+
+	return res
 }
