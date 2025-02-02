651878512167cb6fbe2b24226dcc98e92410cd03
npezzotti <nathan.pezzotti@hashicorp.com>
2025-02-02 11:32:10 -0500

    adds fins cheapest price

---
+++ cheapest-flights-within-k-stops/cheapest_flights_within_k_stops.go
@@ -0,0 +1,32 @@
+package cheapest_flights_within_k_stops_test
+
+import (
+	"fmt"
+	"math"
+)
+
+func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
+	distances := make([]int, n)
+	for i := range distances {
+		distances[i] = math.MaxInt32
+	}
+
+	fmt.Println(distances)
+	distances[src] = 0
+
+	for i := 0; i < k+1; i++ {
+		distancesBackup := make([]int, n)
+		copy(distancesBackup, distances)
+
+		for _, flight := range flights {
+			from, to, cost := flight[0], flight[1], flight[2]
+			distances[to] = min(distances[to], distancesBackup[from]+cost)
+		}
+	}
+
+	if distances[dst] == math.MaxInt32 {
+		return -1
+	}
+
+	return distances[dst]
+}
---
+++ cheapest-flights-within-k-stops/cheapest_flights_within_k_stops_test.go
@@ -0,0 +1,46 @@
+package cheapest_flights_within_k_stops_test
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findCheapestPrice(t *testing.T) {
+	tcases := []struct {
+		flights                [][]int
+		n, src, dst, k, output int
+	}{
+		{
+			n:       4,
+			flights: [][]int{{0, 1, 100}, {1, 2, 100}, {2, 0, 100}, {1, 3, 600}, {2, 3, 200}},
+			src:     0,
+			dst:     3,
+			k:       1,
+			output:  700,
+		},
+		{
+			n:       3,
+			flights: [][]int{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}},
+			src:     0,
+			dst:     2,
+			k:       1,
+			output:  200,
+		},
+		{
+			n:       3,
+			flights: [][]int{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}},
+			src:     0,
+			dst:     2,
+			k:       0,
+			output:  500,
+		},
+	}
+
+	for _, tt := range tcases {
+		if output := findCheapestPrice(
+			tt.n, tt.flights, tt.src, tt.dst, tt.k,
+		); !reflect.DeepEqual(output, tt.output) {
+			t.Errorf("got %v, expected %v", output, tt.output)
+		}
+	}
+}
